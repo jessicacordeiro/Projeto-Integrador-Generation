5c0535f6f58b2dde225aa5077531452b74a41a18
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-02-19 14:05:52 -0300

    SPRING BOOT DO PROJETO

---
+++ meuAtelie/src/main/java/com/meuatelie/meuAtelie/MeuAtelieApplication.java
@@ -0,0 +1,13 @@
+package com.meuatelie.meuAtelie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuAtelieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuAtelieApplication.class, args);
+	}
+
+}
