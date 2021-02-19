456f3f5b8cb6f6ac3a148845a4157500d067928f
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-02-19 15:04:00 -0300

    SPRING BOOT do projeto

---
+++ meuAtelie/src/main/java/br/com/ecommerce/meuAtelie/MeuAtelieApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.meuAtelie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuAtelieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuAtelieApplication.class, args);
+	}
+
+}
