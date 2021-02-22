f01d760fb25d2c7d598ffcab4bd0afadee4ca9ef
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-02-22 19:36:20 -0300

    Criação da tb_usuario

--- meuAtelie/src/main/java/br/com/ecommerce/meuAtelie/model/UsuarioModel.java
+++ meuAtelie/src/main/java/br/com/ecommerce/meuAtelie/model/UsuarioModel.java
@@ -7,11 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
-import javax.websocket.OnError;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
