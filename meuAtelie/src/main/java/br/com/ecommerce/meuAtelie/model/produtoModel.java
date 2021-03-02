2044664b63f124e7d9b24492c12503d8b8c48acb
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-03-02 20:10:03 -0300

    modificação

--- meuAtelie/src/main/java/br/com/ecommerce/meuAtelie/model/produtoModel.java
+++ meuAtelie/src/main/java/br/com/ecommerce/meuAtelie/model/produtoModel.java
@@ -1,7 +1,6 @@
 package br.com.ecommerce.meuAtelie.model;
 
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
