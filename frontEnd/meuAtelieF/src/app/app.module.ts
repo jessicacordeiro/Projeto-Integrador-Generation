14e9a9f4f12b27573d0fb0ff65ec339a41d24274
Anderson Conforto <afcaconforto@gmail.com>
2021-03-22 15:56:59 -0300

    Criação do modal cadastrar

--- frontEnd/meuAtelieF/src/app/app.module.ts
+++ frontEnd/meuAtelieF/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    EntrarComponent
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
