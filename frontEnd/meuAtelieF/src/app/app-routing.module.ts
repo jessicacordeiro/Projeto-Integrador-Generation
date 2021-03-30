bbacb5d06c199544d34878c7c90016d858f8c31b
unknown <g.sdomingues@hotmail.com>
2021-03-30 13:21:24 -0300

    Criação da página artesão

--- frontEnd/meuAtelieF/src/app/app-routing.module.ts
+++ frontEnd/meuAtelieF/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtesaoComponent } from './artesao/artesao.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"entrar",pathMatch:"full"},
+  {path:"entrar",component:EntrarComponent},
+  {path:"cadastroProduto",component:CadastroProdutoComponent},
+  {path:"menu",component:MenuComponent},
+  {path:"artesao",component:ArtesaoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
