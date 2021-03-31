9d65dcb69de7fce995f20ffdcf0acc12462751a3
unknown <g.sdomingues@hotmail.com>
2021-03-31 16:35:13 -0300

    Criação do service auth e modal

---
+++ frontEnd/meuAtelieF/src/app/modal/ProdutoModel.ts
@@ -0,0 +1,10 @@
+export class ProdutoModel {
+    public id: number
+    public nomeProduto: string
+    public descricaoProduto: string
+    public precoProduto: number
+    public quantidadeProduto: number
+    public imagemProduto: string
+    public avaliacaoProduto: string
+    
+    }
---
+++ frontEnd/meuAtelieF/src/app/service/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {UsuarioLogin} from '../modal/UsuarioLogin';
+import {UsuarioModel} from '../modal/UsuarioModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http:HttpClient)  { }
+
+    entrar(usuarioLogin:UsuarioLogin):Observable<UsuarioLogin>{ 
+      return this.http.post<UsuarioLogin>('http://localhost:8080/usuario/logar',usuarioLogin)
+    }
+   
+    cadastrar(usuarioModel:UsuarioModel):Observable<UsuarioModel>{ 
+      return this.http.post<UsuarioModel>('http://localhost:8080/usuario/cadastrar',usuarioModel)
+    }
+
+  }
